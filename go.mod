6de291c5b91d6a26b1911fd01abe85dddd8af51c
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-12-12 23:22:10 +0100

    First test of the MS Graph SDK, not working at all.
    Issues:
    	- Errors from the API not populating the errors in Go
    	- Query parameters not being populated for the request
    	- Takes ages to compile
    
    I've created the following issue in GitHub with the SDK team:
    https://github.com/microsoftgraph/msgraph-sdk-go/issues/52
    
    Let's hope they understand the issues better than me, but this
    feels like a pre-alpha version and shouldn't even be played with.
    
    To be continued...

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/simongottschlag/meh
+
+go 1.17
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v0.12.0
+	github.com/microsoft/kiota/authentication/go/azure v0.0.0-20211203130928-8449c9e67101
+	github.com/microsoftgraph/msgraph-sdk-go v0.3.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.20.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.8.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/microsoft/kiota/abstractions/go v0.0.0-20211202082735-099f3c37853a // indirect
+	github.com/microsoft/kiota/http/go/nethttp v0.0.0-20211203130928-8449c9e67101 // indirect
+	github.com/microsoft/kiota/serialization/go/json v0.0.0-20211112084539-17ac73ffdc7c // indirect
+	github.com/microsoftgraph/msgraph-sdk-go-core v0.0.5 // indirect
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	a "github.com/microsoft/kiota/authentication/go/azure"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/me/calendarview"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "application returned an error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toInt32Ptr(n int32) *int32 {
+	return &n
+}
+
+func toStrPtr(s string) *string {
+	return &s
+}
+
+func run() error {
+	cred, err := azidentity.NewAzureCLICredential(&azidentity.AzureCLICredentialOptions{})
+	if err != nil {
+		return fmt.Errorf("error creating credentials: %w", err)
+	}
+
+	auth, err := a.NewAzureIdentityAuthenticationProviderWithScopes(cred, []string{"https://graph.microsoft.com"})
+	if err != nil {
+		return fmt.Errorf("error authentication provider: %w", err)
+	}
+
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(auth)
+	if err != nil {
+		return fmt.Errorf("error creating adapter: %w", err)
+	}
+
+	client := msgraphsdk.NewGraphServiceClient(adapter)
+
+	startDateTime := "2021-12-12T20:57:37.046Z"
+	endDateTime := "2021-12-19T20:57:37.046Z"
+
+	query := calendarview.CalendarViewRequestBuilderGetQueryParameters{
+		StartDateTime: &startDateTime,
+		EndDateTime:   &endDateTime,
+	}
+
+	options := calendarview.CalendarViewRequestBuilderGetOptions{
+		Q: &query,
+	}
+
+	result, err := client.Me().CalendarView().Get(&options)
+	if err != nil {
+		return fmt.Errorf("get req failed: %w", err)
+	}
+
+	for _, event := range result.GetValue() {
+		subject := event.GetSubject()
+		startTime := event.GetStart()
+		endTime := event.GetEnd()
+		fmt.Printf("%s (%s - %s)\n", *subject, *startTime.GetDateTime(), *endTime.GetDateTime())
+	}
+
+	return nil
+}
